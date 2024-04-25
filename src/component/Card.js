4c8656d1a1306675b1489851e7b36b451402f618
kulkarnianushree <anushreejmkulkarni@gmail.com>
2024-04-25 13:27:25 +0530

    added

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,10 +1,14 @@
 import "./Card.css";
-function Card(props){
-    const classes = "card"+props.className
-    return(
 
-        <div className={classes}>{props.children}</div>
-    )
+function Card(props) {
+    // Ensure that there is a space before appending the className from props
+    const classes = "card " + props.className;
 
+    return (
+        <div className={classes}>
+            {props.children}
+        </div>
+    );
 }
+
 export default Card;
