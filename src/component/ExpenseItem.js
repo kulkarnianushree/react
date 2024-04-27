d3c57084b97ea64fe1cdbd169fbc920f31b9bfa3
kulkarnianushree <anushreejmkulkarni@gmail.com>
2024-04-27 13:52:08 +0530

    useState function is updated

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,20 +1,22 @@
+import React,{useState} from "react"
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 
 function ExpenseItem(props) {
-  function buttonClickhandler(event){
-    console.log("button clicked")
+  const [title,setTitle] = useState(props.title);
+  function buttonClickHandler(event){
+    setTitle("New Title")
 
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
-        <button onClick={buttonClickhandler}>Change title</button>
+        <button onClick="buttonClickHandler">Clicked</button>
       </div>
     </Card>
   );
